ce30fd86001a32cd253ffe3c0d2a5459b92b4dd7
hyoungsun <hyoungsun@uchicago.edu>
2020-07-22 01:44:39 -0400

    Updates

---
+++ precache-manifest.69a97449004cae313a87312c64e53870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f7c69f319c7476d4b3495c3f22f70c9",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "965d72e6229969334ba8",
+    "url": "/movie_app_2020/static/css/main.e083b577.chunk.css"
+  },
+  {
+    "revision": "b4ad6d5796d4c9c3f3c1",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.3507762e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "965d72e6229969334ba8",
+    "url": "/movie_app_2020/static/js/main.54ffb2b3.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
